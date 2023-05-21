1065a6f2ecbedd1e488e5ba91cbb93c8fe7c96ba
mmadfox <sergey.liskonog@gmail.com>
2023-05-21 10:49:21 +0300

    feat: add presets

--- presets.go
+++ presets.go
@@ -1,6 +1,28 @@
 package gpsgen
 
-import "github.com/mmadfox/go-gpsgen/navigator"
+import (
+	"github.com/mmadfox/go-gpsgen/navigator"
+	"github.com/mmadfox/go-gpsgen/random"
+	"github.com/mmadfox/go-gpsgen/route"
+)
+
+func RandomDrone() (*Device, error) {
+	model := random.String(5)
+	routes, err := route.RoutesForRussia()
+	if err != nil {
+		return nil, err
+	}
+	return Drone(model, nil, routes...)
+}
+
+func RandomTracker() (*Device, error) {
+	model := random.String(5)
+	routes, err := route.RoutesForRussia()
+	if err != nil {
+		return nil, err
+	}
+	return Tracker(model, nil, routes...)
+}
 
 func Drone(
 	model string,
@@ -9,9 +31,9 @@ func Drone(
 ) (*Device, error) {
 	return NewDevice(
 		WithModel(model),
-		WithSpeed(5, 10, Amplitude128),
+		WithSpeed(5, 10, Amplitude4),
 		WithRoutes(route),
-		WithElevation(100, 800, Amplitude128),
+		WithElevation(100, 800, Amplitude8),
 		WithBattery(80, 100),
 		WithOffline(1, 10),
 		WithProps(props),
@@ -28,8 +50,8 @@ func DroneWithSensors(
 	return NewDevice(
 		WithModel(model),
 		WithRoutes(routes),
-		WithSpeed(5, 10, Amplitude512),
-		WithElevation(100, 800, Amplitude128),
+		WithSpeed(5, 10, Amplitude8),
+		WithElevation(100, 800, Amplitude16),
 		WithBattery(80, 100),
 		WithSensors(sensors...),
 		WithOffline(1, 10),
@@ -46,7 +68,7 @@ func Tracker(
 	return NewDevice(
 		WithModel(model),
 		WithRoutes(route),
-		WithSpeed(1, 3, Amplitude32),
+		WithSpeed(1, 3, Amplitude4),
 		WithElevation(1, 150, Amplitude8),
 		WithBattery(1, 100),
 		WithOffline(10, 120),
@@ -63,10 +85,10 @@ func TrackerWithSensors(
 ) (*Device, error) {
 	return NewDevice(
 		WithModel(model),
-		WithSpeed(1, 3, 16),
+		WithSpeed(1, 3, Amplitude4),
 		WithRoutes(routes),
 		WithSensors(sensors...),
-		WithElevation(1, 150, 16),
+		WithElevation(1, 150, Amplitude8),
 		WithBattery(1, 100),
 		WithOffline(10, 120),
 		WithProps(props),
